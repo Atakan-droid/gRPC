4e799fb70c6ed82516f12b15e73a942f3afd464f
Atakan Göçer <atakangocer98@gmail.com>
2023-10-26 22:31:35 +0300

    GrpcDemo project added, product and greeter demos added

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //await GreetProtoAsync();
+
+            //await GetProductAsync();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7248");
+            var client = new Product.ProductClient(channel);
+
+            var clientReq = new NewProductModel();
+
+            var product = client.CreateNewProduct(clientReq);
+
+            using var call= client.CreateNewProduct(clientReq);
+
+            while (await call.ResponseStream.MoveNext())
+            {
+                var current= call.ResponseStream.Current;
+                Console.WriteLine(current);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static async Task GetProductAsync()
+        {
+            var input = int.Parse(Console.ReadLine());
+            var channel = GrpcChannel.ForAddress("https://localhost:7248");
+            var client = new Product.ProductClient(channel);
+
+            var model = new ProductLookupModel()
+            {
+                ProductId = input
+            };
+            var reply = await client.GetProductInfoAsync(model);
+            Console.WriteLine(reply.ToString());
+        }
+
+        private static async Task GreetProtoAsync()
+        {
+            Console.WriteLine("Input waiting..");
+            string getName = Console.ReadLine().ToString();
+
+            var input = new HelloRequest()
+            {
+                Name = getName
+            };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7248");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine(reply.ToString());
+        }
+    }
+}
---
+++ GrpcDemo/GrpcServer/Services/ProductService.cs
@@ -0,0 +1,62 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class ProductService : Product.ProductBase
+    {
+        private readonly ILogger<ProductService> _logger;
+
+        public ProductService(ILogger<ProductService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ProductModel> GetProductInfo(ProductLookupModel request, ServerCallContext context)
+        {
+            ProductModel output = new();
+
+            _logger.LogInformation($"Requested: {context.Method}");
+            if (request.ProductId == 1)
+            {
+                output.CreationDate = DateTime.Now.ToShortDateString();
+                output.Desc = "ProductId 1";
+                output.Name = request.ProductId.ToString() + "Product ";
+            }
+            else if (request.ProductId == 2)
+            {
+                output.CreationDate = DateTime.Now.AddDays(-1).ToShortDateString();
+                output.Desc = request.ProductId.ToString();
+                output.Name = request.ProductId.ToString() + "Product";
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException($"ProductId invalid: {request.ProductId.ToString()}");
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task CreateNewProduct(NewProductModel request, IServerStreamWriter<ProductModel> responseStream, ServerCallContext context)
+        {
+            List<ProductModel> products = new List<ProductModel>(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                products.Add(new ProductModel
+                {
+                    Name = "Product " + i.ToString(),
+                    CreationDate = DateTime.Now.AddHours(i).ToShortDateString(),
+                    Desc = "Desc " + i.ToString(),
+                    Quantity = i,
+                    IsActive = true
+                });
+            }
+
+            foreach (var prod in products)
+            {
+                await Task.Delay(2000);
+                await responseStream.WriteAsync(prod);
+            }
+        }
+    }
+}
